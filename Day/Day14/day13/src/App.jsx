69307abb283a9c4b30e72041f9c09e0d08bc76bc
gaganaka27-lab <gaganaka27@gmail.com>
2026-07-30 15:10:19 +0530

    Hooks ex3 : useContext

--- Day/Day14/day13/src/App.jsx
+++ Day/Day14/day13/src/App.jsx
@@ -3,16 +3,19 @@ import SimpleList from "./components/ListOne";
 import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
-
+import UserListHook from "./components/HookTwo";
+import ThemeProviderDemo from "./components/HookThree";
 function App() {
   
   return (
     <>
-      <SimpleList />
+      {/* <SimpleList />
       <UserList />
       <ProductListDemo />
 
-      <CounterHook/>
+      <CounterHook/> */}
+      {/* <UserListHook/> */}
+      <ThemeProviderDemo />
     </>
   )
 }
