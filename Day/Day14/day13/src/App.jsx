6a911e1e948663d0d3f912f7b2385f9637be4220
gaganaka27-lab <gaganaka27@gmail.com>
2026-07-30 13:22:34 +0530

    Day 14 react app : list rendering

---
+++ Day/Day14/day13/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import SimpleList from "./components/ListOne";
+import UserList from "./components/ListTwo";
+import ProductListDemo from "./components/ListThree";
+import CounterHook from "./components/HookOne";
+
+function App() {
+  
+  return (
+    <>
+      <SimpleList />
+      <UserList />
+      <ProductListDemo />
+
+      <CounterHook/>
+    </>
+  )
+}
+
+export default App
